21a36cd5427755164bd7e7750a42c0c131812d10
samx-x777 <sam.sahoo2005@gmail.com>
2024-05-17 17:43:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+let a = "ready";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <li>More</li>
+    </nav>
+    <h1>Hello {a}</h1>
+    <div className="react-container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, saepe aperiam! Consequuntur tempore nam nesciunt architecto harum. Nemo nam, odit ea ut cupiditate laborum repellendus aliquam quam unde numquam, amet id tempora saepe ipsam sint totam placeat! Ad ipsum perspiciatis non qui iure, nobis asperiores suscipit minus. At nisi odit delectus? Iste distinctio numquam accusantium dicta.
     </div>
+    </>
   );
 }
 
